f155e8598c4f73c17c8357956a71ed39a30a4a9c
DawalaDeny <92683913+DawalaDeny@users.noreply.github.com>
2022-11-29 10:08:27 +0100

    Initial commit

---
+++ SmartFunds.Ui.Webapp/Controllers/TransactionsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartFunds.Ui.Webapp.Data;
+
+namespace SmartFunds.Ui.Webapp.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private readonly SmartFundsDbContext _dbContext;
+
+        public TransactionsController(SmartFundsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var transactions = _dbContext.Transactions
+                .Where(t => t.OrganizationId == id)
+                .ToList();
+
+            return View(transactions);
+        }
+    }
+}
